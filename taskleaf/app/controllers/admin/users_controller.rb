7ad8e9efc6b2b3610f7095cc5e0e262f4bf58301
gengogo5 <gen0908@gmail.com>
2019-03-03 00:36:37 +0900

    4-5-4 ユーザ管理(更新・削除)機能追加

--- taskleaf/app/controllers/admin/users_controller.rb
+++ taskleaf/app/controllers/admin/users_controller.rb
@@ -1,6 +1,18 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -8,6 +20,16 @@ class Admin::UsersController < ApplicationController
 
     if @user.save
       redirect_to admin_users_path, notice: "ユーザー「#{@user.name}を登録しました。」"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
     else
       render :new
     end
